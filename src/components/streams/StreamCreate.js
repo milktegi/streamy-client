2de2183df4e9b2b7e70b0c05b584e8d7829ba75e
milktegi <milkteegi@gmail.com>
2019-01-31 10:47:35 +0900

    displayed error messages

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -4,8 +4,37 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-  renderInput = ({ input }) => {
-    return <input {...input} />;
+
+  renderError({ error, touched }){
+    if(touched && error){
+      return (
+        <div className="ui error message">
+          <div className="header">
+            {error}
+          </div>
+        </div>
+      )
+    }
+  }
+
+
+
+  renderInput = ({ input, label, meta }) => {
+    // console.log(meta);
+    const className = `field ${
+      meta.error && meta.touched ?
+      'error'  
+      : ''
+    }`;
+    return (
+      <div className={className} autoComplete="off">
+        <label>{label}</label>
+        <input {...input}/>
+        <div>
+          {this.renderError(meta)}
+        </div>
+      </div>
+    )
   };
 
   onSubmit = userInput => {
@@ -17,9 +46,14 @@ class StreamCreate extends React.Component {
   render() {
     // console.log(props);
     return (
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} 
+      className="ui form error">
+        <Field 
+        label="제목"
+        name="title" component={this.renderInput} />
+        <Field 
+        label="내용"
+        name="description" component={this.renderInput} />
       <button className="ui button primary">submit</button>
       </form>
     );
@@ -32,14 +66,15 @@ const errors = {};
 
    if(!userInput.title){
 // 유저 타이틀 미입력시에만 실행
-    errors.title = '타이틀 입력해주세요';
+    errors.title = '타이틀을 입력해주세요 😮';
    }
    if(!userInput.description){
-     errors.description = '내용을 입력해주세요';
+     errors.description = '내용을 입력해주세요 🤔';
    }
    return errors;
 }
 
 export default reduxForm({
-  form: 'streamCreate'
+  form: 'streamCreate',
+  validate
 })(StreamCreate);
