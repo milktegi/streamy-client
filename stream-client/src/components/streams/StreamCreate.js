137687fb7a0d06cfc0bb41e0f5e9a804189de162
milktegi <milkteegi@gmail.com>
2019-02-01 12:25:14 +0900

    refactored StreamCreate comp

--- stream-client/src/components/streams/StreamCreate.js
+++ stream-client/src/components/streams/StreamCreate.js
@@ -6,40 +6,9 @@ import { Field, reduxForm } from 'redux-form';
 //actions
 import { connect } from 'react-redux';
 import { createStream } from '../../actions';
+import StreamForm from '../streams/StreamForm'
 
 class StreamCreate extends React.Component {
-
-  renderError({ error, touched }){
-    if(touched && error){
-      return (
-        <div className="ui error message">
-          <div className="header">
-            {error}
-          </div>
-        </div>
-      )
-    }
-  }
-
-
-
-  renderInput = ({ input, label, meta }) => {
-    // console.log(meta);
-    const className = `field ${
-      meta.error && meta.touched ?
-      'error'  
-      : ''
-    }`;
-    return (
-      <div className={className} autoComplete="off">
-        <label>{label}</label>
-        <input {...input}/>
-        <div>
-          {this.renderError(meta)}
-        </div>
-      </div>
-    )
-  };
 
   onSubmit = userInput => {
     // e.preventDefault();
@@ -51,37 +20,15 @@ class StreamCreate extends React.Component {
   render() {
     // console.log(props);
     return (
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} 
-      className="ui form error">
-        <Field 
-        label="제목"
-        name="title" component={this.renderInput} />
-        <Field 
-        label="내용"
-        name="description" component={this.renderInput} />
-      <button className="ui button primary">submit</button>
-      </form>
+      <div>
+        <h3>새 스트림 만들기</h3>
+        <StreamForm onSubmit={this.onSubmit}/>
+      </div>
     );
   }
 }
 
-const validate = userInput => {
 
-const errors = {};
 
-   if(!userInput.title){
-// 유저 타이틀 미입력시에만 실행
-    errors.title = '타이틀을 입력해주세요 😮';
-   }
-   if(!userInput.description){
-     errors.description = '내용을 입력해주세요 🤔';
-   }
-   return errors;
-}
 
-const formWrapped = reduxForm({
-  form: 'streamCreate',
-  validate
-})(StreamCreate);
-
-export default connect(null, { createStream })(formWrapped);
+export default connect(null, { createStream })(StreamCreate);
