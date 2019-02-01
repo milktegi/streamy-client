d501fac04c8c857cd4e53b1644dbb718b904f331
milktegi <milkteegi@gmail.com>
2019-02-01 12:00:12 +0900

    handled undefined state in Edit comp

--- stream-client/src/components/streams/StreamEdit.js
+++ stream-client/src/components/streams/StreamEdit.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
+class StreamEdit extends React.Component {
 
-const StreamEdit = () => {
-  return (
-    <div>
-    StreamEdit
-    </div>
-  );
+  componentDidMount() {
+ 
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render() {
+    if (!this.props.stream) {
+      return <div>loading...</div>;
+    } else {
+      return <div>{this.props.stream.title}</div>;
+    }
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  // console.log(ownProps);
+  const { id } = ownProps.match.params;
+  console.log('맵스투', id);
+
+  return {
+    stream: state.streams[id]
+  };
 };
 
-export default StreamEdit;
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamEdit);
