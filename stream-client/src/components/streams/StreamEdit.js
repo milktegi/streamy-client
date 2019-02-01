c52fa82ca5c70d1ea4a0ce94f3bd00b6e8054361
milktegi <milkteegi@gmail.com>
2019-02-01 12:55:56 +0900

    extracted form initialValues only needed + rendered

--- stream-client/src/components/streams/StreamEdit.js
+++ stream-client/src/components/streams/StreamEdit.js
@@ -1,26 +1,42 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
-
   componentDidMount() {
- 
     this.props.fetchStream(this.props.match.params.id);
   }
+
+  //콜백
+  onSubmit = userInput => {
+    console.log(userInput);
+  };
+
   render() {
-    if (!this.props.stream) {
-      return <div>loading...</div>;
-    } else {
-      return <div>{this.props.stream.title}</div>;
-    }
+    // if(!this.props.streams){
+    //   return (
+    //     <div>
+    //       loadidng...
+    //     </div>
+    //   )
+    // }
+
+    return (
+      <div>
+        <h3> 스트림 수정 </h3>
+        <StreamForm
+        initialValues={_.pick(this.props.stream, 'title', 'description')} 
+        onSubmit={this.onSubmit} />
+      </div>
+    );
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
   // console.log(ownProps);
   const { id } = ownProps.match.params;
-  console.log('맵스투', id);
 
   return {
     stream: state.streams[id]
@@ -29,5 +45,5 @@ const mapStateToProps = (state, ownProps) => {
 
 export default connect(
   mapStateToProps,
-  { fetchStream }
+  { fetchStream, editStream }
 )(StreamEdit);
